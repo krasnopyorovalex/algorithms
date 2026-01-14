b91c56540f7229a4e306a5ddd1303987faa0129a
Alexander Krasnopyorov <a.krasnoperov@pfsol.ru>
2026-01-14 12:05:20 +0300

    move bitwise function to BitWise directory

---
+++ BitWise/insert-b-to-a.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+function insertBtoA(int $a, int $b, int $start, int $end): int
+{
+    $maskL = (-1 << ($start + 1));
+    $maskR = (1 << $end) - 1;
+    $maskA = $maskL | $maskR;
+
+    $a &= $maskA;
+
+    $b <<= $end;
+    $maskB = ((1 << ($start + 1)) - 1);
+    $b &= $maskB;
+
+    return $a | $b;
+}
+
+echo insertBtoA(285_689_642, 1_132_478_069, 16, 5), PHP_EOL;
