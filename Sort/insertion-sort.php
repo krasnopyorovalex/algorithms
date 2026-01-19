54df11087e244a6f9d0fc65c143cebaf735c219a
Alexander Krasnopyorov <a.krasnoperov@pfsol.ru>
2026-01-19 16:24:06 +0300

    add insertion-sort

---
+++ Sort/insertion-sort.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+function insertionSort(array $arr): array
+{
+    for ($i = 0, $iMax = count($arr) - 1; $i < $iMax; $i++) {
+        $temp = $arr[$i + 1];
+        $position = $i;
+
+        while ($position >= 0) {
+            if ($arr[$position] > $temp) {
+                $arr[$position + 1] = $arr[$position];
+                $position--;
+            } else {
+                break;
+            }
+
+            $arr[$position + 1] = $temp;
+        }
+    }
+
+    return $arr;
+}
+
+$arr = [2, 100, 0, -9, 55, 80, 6];
+
+print_r(
+    insertionSort($arr)
+);
+
+
