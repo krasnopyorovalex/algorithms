ae09fe0693784f11f41244260190fb0387485cf7
Alexander Krasnopyorov <a.krasnoperov@pfsol.ru>
2026-04-21 11:02:01 +0300

    change to nowdoc on RedisDistributedLock::unLock

--- DistributedLock/Redis/RedisDistributedLock.php
+++ DistributedLock/Redis/RedisDistributedLock.php
@@ -29,7 +29,7 @@ final readonly class RedisDistributedLock implements DistributedLock
 
     public function unLock(string $lockKey, string $lockId): int
     {
-        $luaCommand = <<<LUA
+        $luaCommand = <<<'LUA'
             if redis.call("get", KEYS[1]) == ARGV[1] then
                 return redis.call("del", KEYS[1])
             else return 0 end
