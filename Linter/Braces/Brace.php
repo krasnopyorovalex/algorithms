c4fdb0636821f105f4f8ae9cc8964a38b5bd2079
Alexander Krasnopyorov <a.krasnoperov@pfsol.ru>
2026-04-21 11:02:39 +0300

    add phpstan to Brace.php

--- Linter/Braces/Brace.php
+++ Linter/Braces/Brace.php
@@ -8,11 +8,17 @@ final readonly class Brace
 {
     public function __construct(private(set) string $char) {}
 
+    /**
+     * @param array<int, string> $bracesMap
+     */
     public function isOpen(array $bracesMap): bool
     {
         return array_key_exists($this->char, $bracesMap);
     }
 
+    /**
+     * @param string<non-empty-string> $char
+     */
     public function isValidClose(string $char): bool
     {
         return $char === $this->char;
